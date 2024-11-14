2e45be36bfb597ad33f3c80ac611616959d2fd65
PuzzledStone <ianxd03@gmail.com>
2024-11-13 21:49:15 -0600

    added protected routes

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,40 +1,56 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom"; 
+import React, { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import Sidebar from "../components/UI/Sidebar.jsx";
-import {CourseDetails} from "./CourseDetails";
-import {Events} from "./Events";
-import {HomePage} from "./HomePage";
-import {Statistics} from "./Statistics";
+import { CourseDetails } from "./CourseDetails";
+import { Events } from "./Events";
+import { HomePage } from "./HomePage";
+import { Statistics } from "./Statistics";
 import Modal from 'react-modal';
-import {courses} from "../pages/HomePage.jsx";
+import { courses } from "../pages/HomePage.jsx";
+import ProtectedRoute from './ProtectedRoute'; 
+
 Modal.setAppElement('#root');
 
-export const profileInfo = [
-    {
-        id: 1,
-        img:"https://i.pinimg.com/564x/22/8b/cf/228bcf5a0800f813cd1744d4ccbf01ea.jpg",
-        name:"Ernesto",
-        lastName1:"Jimenez",
-        lastName2:"Castro",
-        mail: "ernesto.jc@gmail.com",
-        usr: "erne12",
-        taskCompleted: 5,
-        taskRemaining: 43,
-        courses: courses
-    }
-];
-
 export function App() {
-    return (    
+
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+n
+    useEffect(() => {
+        const checkAuth = () => {
+            const token = localStorage.getItem('authToken');
+            setIsAuthenticated(!!token); 
+        };
+        checkAuth();
+    }, []);
+
+    return (
         <div className="flex gap-4 bg-clr-light-bg dark:bg-clr-dark-bg w-full">
             <aside className="w-16">
-                <Sidebar username={profileInfo[0].usr} email={profileInfo[0].mail} image={profileInfo[0].img} items={profileInfo}></Sidebar>
+                <Sidebar 
+                    username={profileInfo[0].usr} 
+                    email={profileInfo[0].mail} 
+                    image={profileInfo[0].img} 
+                    items={profileInfo} 
+                />
             </aside>
+
             <Routes>
                 <Route path="/home" element={<HomePage name={profileInfo[0].name} />} />
-                <Route path="/events" element={<Events />} />
-                <Route path="/statistics" element={<Statistics />} />
-                <Route path="/courseDetails" element={<CourseDetails />} />
+    
+                <Route 
+                    path="/events" 
+                    element={<ProtectedRoute element={<Events />} isAuthenticated={isAuthenticated} />} 
+                />
+                <Route 
+                    path="/statistics" 
+                    element={<ProtectedRoute element={<Statistics />} isAuthenticated={isAuthenticated} />} 
+                />
+                <Route 
+                    path="/courseDetails" 
+                    element={<ProtectedRoute element={<CourseDetails />} isAuthenticated={isAuthenticated} />} 
+                />
+
+                {/* Ruta por defecto (en caso de que no se encuentre la ruta especificada) */}
                 <Route path="/*" element={<HomePage name={profileInfo[0].name} />} />
             </Routes>
         </div>
