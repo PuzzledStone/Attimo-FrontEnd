435df626313ca0ae13e08c353ccbd3cbd2539bff
PuzzledStone <ianxd03@gmail.com>
2025-03-25 15:05:08 -0600

    .

--- src/components/UI/InfoButton.jsx
+++ src/components/UI/InfoButton.jsx
@@ -34,7 +34,7 @@ const InfoButton = () => {
         </ul>
         <button
           onClick={() => setModalIsOpen(false)}
-          className="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
+          className="mt-4 px-4 py-2 bg-red-800 text-white rounded-lg hover:bg-red-700"
         >
           Cerrar
         </button>
